2b63e5ec367df000a5e5ca294933a7ecac95a022
Niraj kumar <nirajkumar24seven@gmail.com>
2025-08-02 22:48:27 +0530

    Update first.java

--- java/first.java
+++ java/first.java
@@ -1,16 +1,38 @@
-Problem statement
-Take the length(L) and breadth(B) of the rectangle as input and find its area.
+// Problem statement
+// Take the length(L) and breadth(B) of the rectangle as input and find its area.
 
-Note:
-Length and breadth must be an integer value and the area will always be in the range of integers.
-Detailed explanation ( Input/output format, Notes, Images )
-Constraints:
-1 <= L, B <= 10ˆ2
-Sample Input 1:
-4 20
-Sample Output 1:
-80
-Explanation of Sample Input 1:
-Length of the rectangle is 4 and breadth is 20. 
-Hence the area of the rectangle is (length*breadth). 
-So the answer is 4*20=80.
+// Note:
+// Length and breadth must be an integer value and the area will always be in the range of integers.
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints:
+// 1 <= L, B <= 10ˆ2
+// Sample Input 1:
+// 4 20
+// Sample Output 1:
+// 80
+// Explanation of Sample Input 1:
+// Length of the rectangle is 4 and breadth is 20. 
+// Hence the area of the rectangle is (length*breadth). 
+// So the answer is 4*20=80.
+
+
+
+
+
+
+  import java.util.Scanner;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		Scanner sc = new Scanner(System.in);
+
+		int a=sc.nextInt();
+		
+		int b=sc.nextInt();
+		int area=a*b;
+		System.out.println(area);
+		
+	}
+}
